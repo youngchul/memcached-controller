32a23c52f9b00b4f9df72bc96aa7475e5921b987
Youngchul Bang <youngchul@gmail.com>
2019-08-20 11:17:28 +0900

    add Memcached in cache/v1alpha1 API
    This includes boilerplate created from the following command:
    kubebuilder create api --group cache --version v1alpha1 --kind Memcached

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	cachev1alpha1 "github.com/youngchul/memcached-controller/api/v1alpha1"
+	"github.com/youngchul/memcached-controller/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = cachev1alpha1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -58,6 +61,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.MemcachedReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Memcached"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Memcached")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
